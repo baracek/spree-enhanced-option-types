08de98f45e5051b9d365b437210378cd8f4ff53d
Bara Cek <baracek@yahoo.com>
2011-07-12 23:47:01 -0400

    Accidentally moved the update_price route into the admin namespace, moved it back

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :products do
-      
+  resources :products do
       member do
         get :update_price
       end
-      
+  end
+  
+  namespace :admin do
+    resources :products do
       resources :variants do
         collection do
           post :regenerate
