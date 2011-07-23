c647d2b0962796927cb6da67526a1c585c616978
Bara Cek <baracek@yahoo.com>
2011-07-22 22:38:39 -0400

    Stack overflow when adding a product to the cart, due to the alias method chain, not sure why this wasn't shown in development only appeared in production

--- app/controllers/orders_controller_decorator.rb
+++ app/controllers/orders_controller_decorator.rb
@@ -1,5 +1,5 @@
 OrdersController.class_eval do
-  def populate_with_option_types
+  def populate
     params[:option_values].each_pair do |product_id, otov|
       quantity = params[:quantity].to_i if !params[:quantity].is_a?(Array)
       quantity = params[:quantity][variant_id].to_i if params[:quantity].is_a?(Array)
@@ -7,10 +7,21 @@ OrdersController.class_eval do
       params[:variants] = Hash[ Variant.by_option_value_ids(option_value_ids, product_id).first.id, quantity ]
     end if params[:option_values]
 
-    populate_without_option_types
-  end
+    @order = current_order(true)
 
-  alias_method_chain :populate, :option_types
+    params[:products].each do |product_id,variant_id|
+      quantity = params[:quantity].to_i if !params[:quantity].is_a?(Hash)
+      quantity = params[:quantity][variant_id].to_i if params[:quantity].is_a?(Hash)
+      @order.add_variant(Variant.find(variant_id), quantity) if quantity > 0
+    end if params[:products]
+
+    params[:variants].each do |variant_id, quantity|
+      quantity = quantity.to_i
+      @order.add_variant(Variant.find(variant_id), quantity) if quantity > 0
+    end if params[:variants]
+
+    respond_with(@order) { |format| format.html { redirect_to cart_path } }
+  end
 
   respond_override :create => { :html => { :failure => :redirector_for_failure } }
     
